ccf4ab503162083b27ce76d16075442105e346ab
Eric Slyfield <eric.slyfield@gmail.com>
2024-04-23 10:25:42 -0400

    Posted Media and Tweets
    Added ability to post media with tweets. Tested and confirmed working. Cleaned up keys. Added clearer comments.

--- unarchival.py
+++ unarchival.py
@@ -9,11 +9,8 @@ load_dotenv('.env')
 # Keys
 api_key: str = os.getenv('API_KEY')
 api_secret: str = os.getenv('API_SECRET')
-consumer_key: str = os.getenv('API_KEY')
-consumer_secret: str = os.getenv('API_SECRET')
 access_token: str = os.getenv('ACCESS_TOKEN')
 access_secret: str = os.getenv('ACCESS_SECRET')
-access_token_secret: str = os.getenv('ACCESS_SECRET')
 bearer_token: str = os.getenv('BEARER_TOKEN')
 
 # v1.1 Endpoint Access
@@ -25,14 +22,12 @@ api = tweepy.API(auth)
 # v2 Endpoint Access
 client = tweepy.Client(
     bearer_token,
-    consumer_key,
-    consumer_secret,
+    api_key,
+    api_secret,
     access_token,
-    access_token_secret,
+    access_secret,
     wait_on_rate_limit=True,
 )
-
-#client = tweepy.Client(bearer_token)
 
 # Error Check
 try:
@@ -41,16 +36,17 @@ try:
 except:
     print("Oops. Failed to connect to Twitter!")
 
-# A link to your new Tweet
-new_tweet = "A computer can never be held accountable, therefore a computer must never make a management decision. (Sent from @code w/ Python, Tweepy, and Twitter APIv2)"
+# Your Tweet Logic
+new_tweet = "test. (Sent from @code w/ Python, Tweepy, and Twitter APIv2)"
 
-# A link to your new Tweet
-#print(f"https://twitter.com/user/status/{new_tweet.data['id']}")
+# Upload image to Twitter. Replace 'filename' your image filename.
+media_id = api.media_upload(filename="bigthree.png").media_id_string
+print(media_id)
 
-# Try posting the tweet
+# Post your Tweet!
 try:
     # Send Tweet with Text and media ID
-    client.create_tweet(text=new_tweet)
+    client.create_tweet(text=new_tweet, media_ids=[media_id])
     print("Tweet posted successfully!")
 except:
     print("Error occurred while posting the tweet: " + str(new_tweet))
